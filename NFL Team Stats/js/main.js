b521f45ca486b28d3d95ee7690f4d4fb3e790f0c
Harrison Holsinger <harrisonholsinger@gmail.com>
2023-05-08 13:58:53 -0400

    Update main.js

--- NFL Team Stats/js/main.js
+++ NFL Team Stats/js/main.js
@@ -10,19 +10,21 @@ fetch("https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/win-stats/2020",
     .then(response => response.json())
     .then(data => {
         const teamWinStatsList = data["_embedded"]["teamWinStatsList"];
-
-        // Populate the table with the team statistics
+        console.log(teamWinStatsList); // Log the teamWinStatsList object to the console
         const tableBody = document.querySelector("#team-stats-table tbody");
         teamWinStatsList.forEach(team => {
+            // Remove "xy" from the team name using the replace() method
+            const teamName = team.name.replace(/xy|xz|\*xz|\*$/gi, "");
             const row = tableBody.insertRow();
             const nameCell = row.insertCell();
             const winsCell = row.insertCell();
             const lossesCell = row.insertCell();
             const winRateCell = row.insertCell();
-            nameCell.textContent = team.name;
+            nameCell.textContent = teamName;
             winsCell.textContent = team.wins;
             lossesCell.textContent = team.losses;
             winRateCell.textContent = team.winRatePercentage * 100 + "%";
         });
     })
     .catch(err => console.error(err));
+
